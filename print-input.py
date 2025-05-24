9d9cfd19805ebf442a23d439191d308adb638bb2
Nikks <nikksanirudh@gmail.com>
2025-05-24 17:27:09 +0530

    Update print-input.py

--- print-input.py
+++ print-input.py
@@ -1,6 +1,5 @@
-class DummyDataReader(CalibrationDataReader):
-    def __init__(self):
-        self.data_iter = iter([
-            {"input.1": np.random.rand(1, 3, 416, 416).astype(np.float32)}
-            for _ in range(100)
-        ])
+import onnx
+
+model = onnx.load("yolov4_tiny.onnx")
+input_all = [node.name for node in model.graph.input]
+print("Input names:", input_all)
