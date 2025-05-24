b6827b6c1496cd58d0496f70b69e0a3c0ae37002
aiznikks <nikksanirudh@gmail.com>
2025-05-25 02:08:35 +0530

    new

---
+++ models-conversion/ctdet_keras/export.py
@@ -0,0 +1,18 @@
+from keras_centernet.models.networks.hourglass import HourglassNetwork
+
+heads = {
+    'hm': 80,
+    'reg': 2,
+    'wh': 2
+}
+
+kwargs = {
+    'num_stacks': 2,
+    'cnv_dim': 256,
+    'weights': 'ctdet_coco',
+    'inres': (512, 512),
+}
+
+model = HourglassNetwork(heads=heads, **kwargs)
+model.save("centernet_saved_model")
+print("Keras model exported as 'centernet_saved_model'")
