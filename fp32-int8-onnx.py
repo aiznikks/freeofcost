9ca08f47e2e15cb18d3e85254b64f144fc0f732f
Nikks <nikksanirudh@gmail.com>
2025-05-24 18:12:16 +0530

    Update fp32-int8-onnx.py

--- fp32-int8-onnx.py
+++ fp32-int8-onnx.py
@@ -3,19 +3,20 @@ import numpy as np
 
 class DummyDataReader(CalibrationDataReader):
     def __init__(self):
-        self.data_iter = iter([
-            {"input.1": np.random.rand(1, 3, 416, 416).astype(np.float32)}
+        self.data = [
+            {"input.1": np.random.uniform(0, 1, size=(1, 3, 416, 416)).astype(np.float32)}
             for _ in range(100)
-        ])
+        ]
+        self.iterator = iter(self.data)
 
     def get_next(self):
-        return next(self.data_iter, None)
+        return next(self.iterator, None)
 
 quantize_static(
     model_input="yolov4_tiny.onnx",
     model_output="yolov4_tiny_int8.onnx",
     calibration_data_reader=DummyDataReader(),
-    quant_format=QuantType.QOperator,
+    quant_format="QOperator",
     activation_type=QuantType.QUInt8,
     weight_type=QuantType.QInt8
 )
