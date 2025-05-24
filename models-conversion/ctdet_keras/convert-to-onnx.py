b2da541ae436326d65dd23b5d7d9a26698dbce56
Nikks <nikksanirudh@gmail.com>
2025-05-25 02:30:32 +0530

    Update convert-to-onnx.py

--- models-conversion/ctdet_keras/convert-to-onnx.py
+++ models-conversion/ctdet_keras/convert-to-onnx.py
@@ -1,14 +1,1 @@
-import tf2onnx
-import tensorflow as tf
-
-# Path to your saved model
-saved_model_dir = "centernet_saved_model"
-
-# Convert
-model_proto, _ = tf2onnx.convert.from_saved_model(
-    saved_model_dir,
-    output_path="centernet_fp32.onnx",
-    opset=13
-)
-
-print("✅ Exported float32 ONNX model: centernet_fp32.onnx")
+python3 -m tf2onnx.convert --saved-model centernet_saved_model --output centernet_fp32.onnx --opset 13
