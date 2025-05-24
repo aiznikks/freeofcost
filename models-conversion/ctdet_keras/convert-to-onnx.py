88bd495d9e9cf2b0cb327c6f8994e34729bc8dd8
Nikks <nikksanirudh@gmail.com>
2025-05-25 03:09:00 +0530

    Update convert-to-onnx.py

--- models-conversion/ctdet_keras/convert-to-onnx.py
+++ models-conversion/ctdet_keras/convert-to-onnx.py
@@ -1,1 +1,3 @@
 python3 -m tf2onnx.convert --saved-model centernet_saved_model --output centernet_fp32.onnx --opset 13
+
+python3 -m tf2onnx.convert --saved-model centernet_saved_model --output centernet_fp32.onnx --opset 13 --large_model
