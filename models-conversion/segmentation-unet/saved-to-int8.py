515adeea35416d1e5752ab40d14d619355a9a508
Nikks <nikksanirudh@gmail.com>
2025-05-25 14:10:58 +0530

    Create saved-to-int8.py

---
+++ models-conversion/segmentation-unet/saved-to-int8.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+
+def representative_data_gen():
+    for _ in range(100):
+        dummy_input = np.random.rand(1, 128, 128, 16).astype(np.float32)
+        yield [dummy_input]
+
+converter = tf.lite.TFLiteConverter.from_saved_model("unet_saved_model")
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_data_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8
+converter.inference_output_type = tf.int8
+
+tflite_model = converter.convert()
+with open("unet_int8.tflite", "wb") as f:
+    f.write(tflite_model)
