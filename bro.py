d55e1af62cef2bafab2c3ad374057a142ede8472
Nikks <nikksanirudh@gmail.com>
2025-05-27 20:48:58 +0530

    Update bro.py

--- bro.py
+++ bro.py
@@ -1,1 +1,30 @@
-https://raw.githubusercontent.com/Lornatang/FSRCNN-PyTorch/master/model.py
+import torch.nn as nn
+
+class FSRCNN(nn.Module):
+    def __init__(self, upscale_factor):
+        super(FSRCNN, self).__init__()
+        self.first_part = nn.Sequential(
+            nn.Conv2d(1, 56, 5, 1, 2),
+            nn.PReLU(56)
+        )
+
+        self.mid_part = nn.Sequential(
+            nn.Conv2d(56, 12, 1),
+            nn.PReLU(12),
+            nn.Conv2d(12, 12, 3, 1, 1),
+            nn.PReLU(12),
+            nn.Conv2d(12, 12, 3, 1, 1),
+            nn.PReLU(12),
+            nn.Conv2d(12, 12, 3, 1, 1),
+            nn.PReLU(12),
+            nn.Conv2d(12, 56, 1),
+            nn.PReLU(56)
+        )
+
+        self.last_part = nn.ConvTranspose2d(56, 1, 9, stride=upscale_factor, padding=3, output_padding=upscale_factor - 1)
+
+    def forward(self, x):
+        x = self.first_part(x)
+        x = self.mid_part(x)
+        x = self.last_part(x)
+        return x
