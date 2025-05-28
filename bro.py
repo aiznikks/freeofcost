d51bd6c073420d6647d36ab2b7993bfdb503d91c
Nikks <nikksanirudh@gmail.com>
2025-05-28 21:42:07 +0530

    Update bro.py

--- bro.py
+++ bro.py
@@ -1,8 +1,25 @@
-from onnxruntime.quantization import quantize_static, CalibrationDataReader, QuantType
+import os
+import numpy as np
+from PIL import Image
+from onnxruntime.quantization import CalibrationDataReader
 
-quantize_static(
-    model_input="yolov4-tiny.onnx",
-    model_output="yolov4-tiny-int8.onnx",
-    calibration_data_reader=my_data_reader,
-    quant_format=QuantType.QInt8
-)
+class YOLOv4TinyDataReader(CalibrationDataReader):
+    def __init__(self, image_dir):
+        self.image_paths = [
+            os.path.join(image_dir, f)
+            for f in os.listdir(image_dir)
+            if f.lower().endswith((".jpg", ".jpeg", ".png"))
+        ]
+        self.index = 0
+
+    def get_next(self):
+        if self.index >= len(self.image_paths):
+            return None
+
+        img = Image.open(self.image_paths[self.index]).resize((416, 416))
+        img = np.asarray(img).astype(np.float32) / 255.0
+        img = np.transpose(img, (2, 0, 1))  # HWC to CHW
+        img = np.expand_dims(img, axis=0)  # Add batch dim
+
+        self.index += 1
+        return {"000_net": img}
