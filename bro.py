eb65194d518266218ca35a687f1880c6a36d9a88
Nikks <nikksanirudh@gmail.com>
2025-06-09 11:13:55 +0530

    Update bro.py

--- bro.py
+++ bro.py
@@ -1,3 +1,4 @@
+#to freeze model
 import tensorflow as tf
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
@@ -13,4 +14,23 @@ with tf.io.gfile.GFile("frozen_model.pb", "wb") as f:
     
     
     
+#to check input output or saved model
 saved_model_cli show --dir /path/to/saved_model_folder --all
+
+
+
+#
+[one-import-tf]
+input_path=/path/to/saved_model_folder
+output_path=resnet_v2_50.circle
+input_arrays=serving_default_inputs
+output_arrays=StatefulPartitionedCall
+input_shapes=serving_default_inputs[1,224,224,3]
+
+[one-optimize]
+input_path=resnet_v2_50.circle
+output_path=resnet_v2_50_optimized.circle
+
+[one-codegen]
+input_path=resnet_v2_50_optimized.circle
+output_path=resnet_v2_50.tvn
